5e5730d3241558dd4cda786cc322c6f483a2ba14
hanks <hanks6160@gmail.com>
2023-02-22 17:55:26 +0900

    Initial commit

---
+++ src/test/java/kr/co/biztax/wms/WmsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.biztax.wms.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
